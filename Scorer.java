28700e339199a26fca3be978714fccb1cb182d3c
an20olives <elitearo@gmail.com>
2020-04-17 19:44:40 -0400

    Update Scorer.java

--- Scorer.java
+++ Scorer.java
@@ -18,10 +18,13 @@ public class scorer {
     } 
         
     public static void getInput(){
+        // sets up the scanner to accept user input. 
         Scanner scan = new Scanner(System.in);
-        double[][] scores = new double[3][3]; //initialize the 2D Array
+        // sets up the 2D array
+        double[][] scores = new double[3][3];
         int x=0;
         
+       // while loop to get user input and store them in the array.
        while(x!=3){
            int i = 0;
            while(i != 3){
@@ -33,19 +36,26 @@ public class scorer {
        }
       scoreAverage(scores);
     }
+    
+    
     public static void scoreAverage(double[][] a){
-        for(int i = 0; i < 3; i++)
-       {
-            double average = 0; // init variable to hold the average of each row
-            for(int j = 0; j < 3; j++) //re-using our test print code to loop through each row
+        
+        // for loop to find the average of each loop in the array. 
+        for(int i = 0; i < 3; i++){
+            
+            //holds the average of each row
+            double average = 0;
+            for(int j = 0; j < 3; j++)
             {
                 average = average + a[i][j];
             }
         }
         printAverage();
     }
+    
+    // We need to get printAverage to take everything from the other methods and print each row and the average without repeating.
     public static void printAverage(){
         System.out.format("The average of row %d is %5.2f\n\n",(i+1), average);
-
+    // our problem is here
     }
 }
